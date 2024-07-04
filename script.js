052f83d0e5eba866d7107d0f22e032b73a240e1a
Arbaj Ansari <83424578+ArbajAr7@users.noreply.github.com>
2024-07-04 15:18:24 +0545

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,24 @@
-// complete this js code
-class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-
-  greet() {
-    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
-  }
+function Person(name, age) {
+	this.name=name;
+	this.age=age;
 }
 
-class Employee extends Person {
-  constructor(name, age, jobTitle) {
-    super(name, age);
-    this.jobTitle = jobTitle;
-  }
+Person.prototype.greet = function() {
+  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+};
 
-  jobGreet() {
-    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old and my job title is ${this.jobTitle}.`);
-  }
+function Employee(name, age, jobTitle) {
+	Person.call(this, name, age);
+	  this.jobTitle = jobTitle;
 }
 
-let alice = new Person("Alice", 25);
-alice.greet();
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
 
-let bob = new Employee("Bob", 30, "Manager");
-bob.jobGreet();
+Employee.prototype.jobGreet = function() {
+  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+};
+var john = new Employee("Bob", 30, "Manager");
 
-// Do not change code below this line
-window.Person = Person;
-window.Employee = Employee;
+john.greet();
+john.jobGreet();
